d368c1b477792e1411d3c222099d7cc4a49393af
ikruglov <ignatykruglov@gmail.com>
2023-08-07 13:26:51 +0300

    init commit

---
+++ src/test/java/com/ignatykruglov/conveyor/ConveyorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ignatykruglov.conveyor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConveyorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
